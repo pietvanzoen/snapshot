7081486227500b55a8b89af1f02bd4d640b96cb4
Piet van Zoen <piet@cozy.co>
2014-07-22 17:57:21 -0700

    heroku setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express'),
     io = require('socket.io')(http),
     uid = require('rand-token').uid;
 
+var port = Number(process.env.PORT || 4444);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -30,6 +31,6 @@ io.on('connection', function (socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
